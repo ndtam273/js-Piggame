6b22e3e1c590d4bbbc61a1ff4a24ae9e10d2c2a1
tam nguyen <ndtam.273@gmail.com>
2019-11-19 22:42:18 +0700

    add logic for roll button

--- app.js
+++ app.js
@@ -21,6 +21,12 @@ var x = document.querySelector('#score-0').textContent;
 console.log(x);
 
 document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
 
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
@@ -31,8 +37,33 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     var diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png';
-    document.querySelector('#current-' + activePlayer).textContent = dice;
+    
     
     // update the round score if rolled number was not a 1
+    if (dice > 1) {
+        //add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        
+    } else {
+        // next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+        
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        
+        if (activePlayer === 1) {
+             document.querySelector('.player-0-panel').classList.remove('active');
+             document.querySelector('.player-1-panel').classList.add('active');
+        } else {
+             document.querySelector('.player-1-panel').classList.remove('active');
+             document.querySelector('.player-0-panel').classList.add('active');
+        }
+        document.querySelector('.dice').style.display = 'none';
+       
+    }
+    
+    
     
 });
